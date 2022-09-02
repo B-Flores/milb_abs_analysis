453ff1d4d01b5e8d02246695102f5071d668cdec
Brandon Flores <brandonflores.nv@gmail.com>
2022-09-01 22:43:46 -0700

    Initial analysis of swinging strikes on balls over all balls thrown for each day of the 2022 PCL season.

--- swinging_strikes.R
+++ swinging_strikes.R
@@ -8,16 +8,72 @@ FROM data.data
 WHERE details_description = 'Swinging Strike'
 ;
 "
-ss <- dbGetQuery(con, ss_query)
-before_abs <- ss %>%
-  filter(game_date < '2022-05-18')
-after_abs <- ss %>%
-  filter(game_date >= '2022-05-18')
+temp <- dbGetQuery(con, ss_query) %>%
+  group_by(game_date) %>%
+  mutate(is_ball = case_when(pitch_data_coordinates_p_x - 0.95 > 0 |
+                              -0.95 - pitch_data_coordinates_p_x > 0 |
+                              pitch_data_coordinates_p_z - pitch_data_strike_zone_top > 0 |
+                              pitch_data_strike_zone_bottom - pitch_data_coordinates_p_z > 0 ~ "ball",
+                              TRUE ~ "strike")) %>%
+  tabyl(game_date, is_ball) 
 
-swing_at_ball_before <- before_abs %>%
-  filter(pitch_data_coordinates_p_x > 0.95 | pitch_data_coordinates_p_x < -0.95 | 
-           pitch_data_coordinates_p_z > 3.47 | pitch_data_coordinates_p_z < 1.5)
+temp2 <- temp %>% mutate(diff = ball - strike)
 
-swing_at_ball_after <- after_abs %>%
-  filter(pitch_data_coordinates_p_x > 0.95 | pitch_data_coordinates_p_x < -0.95 | 
-           pitch_data_coordinates_p_z > 3.47 | pitch_data_coordinates_p_z < 1.5)
+ggplot(data = temp2)+
+  geom_point(aes(x = game_date, y = diff))
+  
+
+time_series <- ts(data = temp, start = decimal_date(ymd("2022-04-06")),
+                  frequency = 365 / 365)
+
+plot(time_series)
+ 
+
+# percentage of balls swung on over time
+
+balls_query <- "
+SELECT *
+FROM data.data
+WHERE details_description = 'Swinging Strike'
+OR details_description = 'Ball'
+;
+"
+balls <- dbGetQuery(con, balls_query) %>%
+  group_by(game_date) %>%
+  mutate(is_ball = case_when(pitch_data_coordinates_p_x - 0.95 > 0 |
+                               -0.95 - pitch_data_coordinates_p_x > 0 |
+                               pitch_data_coordinates_p_z - pitch_data_strike_zone_top > 0 |
+                               pitch_data_strike_zone_bottom - pitch_data_coordinates_p_z > 0 ~ "ball",
+                             TRUE ~ "strike")) %>%
+  filter(is_ball == "ball") %>%
+  tabyl(game_date, details_description) %>%
+  clean_names() 
+
+swing_percent <- balls %>%
+  mutate(swing_rat = swinging_strike / (ball + swinging_strike)) %>%
+  filter(swing_rat > 0.1125, swing_rat < 0.2) %>%
+  select(swing_rat) 
+
+swing_percent <- rowid_to_column(swing_percent,"index")
+sp1 <- swing_percent %>% filter(index < 37)
+sp2 <- swing_percent %>% filter(index >= 37)
+
+plot(x = sp1$index, y = sp1$swing_rat,
+     xlab = "Day", ylab = "Swing Percent")
+
+abline(lm(swing_rat ~ index,data=sp1),col='red')
+
+plot(x = sp2$index, y = sp2$swing_rat,
+     xlab = "Day", ylab = "Swing Percent")
+
+abline(lm(swing_rat ~ index,data=sp2),col='red')
+# lin_mod <- lm(index ~ swing_rat, data = swing_percent) 
+# plot(lin_mod)  
+  
+
+ggplot(data = swing_percent) +
+  geom_point(aes(x = game_date, y = swing_rat))
+
+model <- ts(data = swing_percent, start = 1,
+            end = 111)
+plot(model)
